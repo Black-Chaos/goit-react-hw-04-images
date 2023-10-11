57f9d2d2f235e1fb819b67ab1f20e2d342bdf7bc
Black Chaos <black.chaos.no1@gmail.com>
2023-10-11 14:48:13 +0300

    fix: fix dependens of effect in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,19 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-export function Modal({ link, tags,handleCloseModal }) {
-    useEffect(() => {
-      document.addEventListener('keydown', handleClose)
-      return () => document.removeEventListener('keydown', handleClose);
-      }, [])
-
+export function Modal({ link, tags, handleCloseModal }) {
+  useEffect(() => {
     const handleClose = ({ code }) => {
-        if (code === 'Escape') handleCloseModal()
-    }
+      if (code === 'Escape') handleCloseModal();
+    };
+    document.addEventListener('keydown', handleClose);
+    return () => document.removeEventListener('keydown', handleClose);
+  }, [handleCloseModal]);
 
-        return (
-      <div className="Overlay">
-        <div className="Modal">
-          <img src={link} alt={tags} />
-        </div>
+  return (
+    <div className="Overlay">
+      <div className="Modal">
+        <img src={link} alt={tags} />
       </div>
-    );
+    </div>
+  );
 }
